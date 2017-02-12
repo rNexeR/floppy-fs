89aa464e57e2d2b814213410ef6193e930a772ad
AstroThing <AstroThing@users.noreply.github.com>
2017-02-12 01:41:57 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 nasm bootloader.asm -f bin -o bootloader.bin
 nasm kernel.asm -f bin -o kernel.bin
-dd if=bootloader.bin of=boot.img bs=512 count=1
-dd if=kernel.bin of=boot.img seek=1
+dd if=bootloader.bin of=boot.img bs=512 count=2880
+dd if=kernel.bin of=boot.img bs=512 seek=1
 qemu-system-x86_64 -fda boot.img 
